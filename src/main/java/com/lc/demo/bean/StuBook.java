0679c26bef69096e5709e3590a129ef81440d2b5
zhangchuang <zhangchuang@reyun.com>
2024-04-20 21:37:53 +0800

    导出

--- src/main/java/com/lc/demo/bean/StuBook.java
+++ src/main/java/com/lc/demo/bean/StuBook.java
@@ -1,8 +1,13 @@
 package com.lc.demo.bean;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class StuBook implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
 
     private String stuId;
 
@@ -40,12 +45,44 @@ public class StuBook implements Serializable {
         this.content = content;
     }
 
+    public StuBook(Integer id, String stuId, String subName, String content) {
+        this.id = id;
+        this.stuId = stuId;
+        this.subName = subName;
+        this.content = content;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
-        return "{" +
-                "stuId='" + stuId + '\'' +
+        return "StuBook{" +
+                "id=" + id +
+                ", stuId='" + stuId + '\'' +
                 ", subName='" + subName + '\'' +
                 ", content='" + content + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StuBook stuBook = (StuBook) o;
+        return Objects.equals(id, stuBook.id) && Objects.equals(stuId, stuBook.stuId) && Objects.equals(subName, stuBook.subName) && Objects.equals(content, stuBook.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, stuId, subName, content);
+    }
+
+    public StuBook() {
     }
 }
